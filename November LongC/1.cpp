f59db0e62e2b7401cc6ee734e2788fc744809d3e
gauravvsgithub <54109748+gauravvsgithub@users.noreply.github.com>
2020-12-13 08:53:03 +0530

    Add files via upload

---
+++ November LongC/1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int displayTime()
+{
+    int n;
+    cin>>n;
+    int dish[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>dish[i];
+    }
+    sort(dish,dish+n);
+    int i=0,j=n-1;
+    int time = 0;
+    while(i<j)
+    {
+        dish[i]--;
+        dish[j]--;
+        if(i==0) i++;
+    
+        if(j==0) j--;
+        time++;
+    }
+    cout<<time;
+
+
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t>0)
+    {
+        displayTime();
+        t--;
+    }
+    return 0;
+}
